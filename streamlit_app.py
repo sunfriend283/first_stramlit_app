bc29c4a025754d6b798f94de9ba228d75c8ec676
Mellyme! MeInyk <119058689+sunfriend283@users.noreply.github.com>
2022-11-25 19:57:21 +0530

    Update streamlit_app.py
    Added filter list for fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,18 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
+streamlit.dataframe(my_fruit_list)
